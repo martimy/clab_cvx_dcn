b22ec4e3a4dac35dbf27d05ecb7edb6380ee2c31
Maen Artimy <artimym@gmail.com>
2022-11-02 18:09:02 +0000

    replace path

--- suzieq/start.sh
+++ suzieq/start.sh
@@ -1,6 +1,7 @@
+SQPATH=/home/vagrant/myclabs/cvx_dcn/suzieq
 docker run --rm -it -p 8501:8501 \
-  -v /home/vagrant/suzieq/dbdir:/home/suzieq/parquet \
-  -v /home/vagrant/suzieq/inventory.yml:/home/suzieq/inventory.yml \
-  -v /home/vagrant/suzieq/my-config.yml:/home/suzieq/my-config.yml \
+  -v $SQPATH/dbdir:/home/suzieq/parquet \
+  -v $SQPATH/inventory.yml:/home/suzieq/inventory.yml \
+  -v $SQPATH/my-config.yml:/home/suzieq/my-config.yml \
   --name sq-poller --network=clab \
   netenglabs/suzieq:latest
