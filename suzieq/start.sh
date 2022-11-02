b652ee70c93725cd5898e5f3278bd53bda4bd0c1
Maen Artimy <artimym@gmail.com>
2022-11-02 19:13:40 +0000

    delete

--- suzieq/start.sh
+++
@@ -1,7 +0,0 @@
-SQPATH=/home/vagrant/myclabs/cvx_dcn/suzieq
-docker run --rm -it -p 8501:8501 \
-  -v $SQPATH/dbdir:/home/suzieq/parquet \
-  -v $SQPATH/inventory.yml:/home/suzieq/inventory.yml \
-  -v $SQPATH/my-config.yml:/home/suzieq/my-config.yml \
-  --name sq-poller --network=clab \
-  netenglabs/suzieq:latest
