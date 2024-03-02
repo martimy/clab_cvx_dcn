e0a2eeeac1bf3ac977eb2c5dfd4459706e7288ae
Maen Artimy <artimym@gmail.com>
2024-03-02 13:50:13 +0000

    use bridge interface

--- reset-dc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Remove all switches
-ovs-vsctl --if-exists del-br switch01
-ovs-vsctl --if-exists del-br switch02
-ovs-vsctl --if-exists del-br switch03
--- setup-dc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Create switches
-for BR in switch01 switch02 switch03
-do
-  echo Create switch $BR
-  ovs-vsctl --may-exist add-br $BR
-done
